e2f1955bb452440ee84f2ab24afed23aacbea0ce
Gaurav Dubey <gauravd638@gmail.com>
2020-10-25 03:55:28 +0530

    Home background set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import "./Home.css"
+import React from 'react';
+import "./Home.css";
 function Home() {
     return (
         <div className='home'>
-            <p> Home componet</p>
+            <div className="home__container">
+                <img className="home__image" src="https://i.gadgets360cdn.com/large/amazon_great_indian_sale_2020_prime_members_january_18_1579305871538.jpg?output-quality=80&output-format=webp" alt=""/>
             
+            <div className="home__row">
+                {/* Product */}
+            </div>
+
+
+
+            </div>
         </div>
-    )
+    );
 }
-import "./Home.css"
-export default Home 
+
+export default Home;
