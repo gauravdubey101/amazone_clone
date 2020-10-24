ff454af2e8f4fbab26d7e6498ac717cae4da0001
Gaurav Dubey <gauravd638@gmail.com>
2020-10-25 03:19:01 +0530

    Home componet begins

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.css"
+function Home() {
+    return (
+        <div className='home'>
+            <p> Home componet</p>
+            
+        </div>
+    )
+}
+import "./Home.css"
+export default Home 
