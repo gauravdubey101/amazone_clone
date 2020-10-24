3a40e7e32271c4ad5e6134261df972c13c0fa7f1
Gaurav Dubey <gauravd638@gmail.com>
2020-10-25 00:44:36 +0530

    Scratch cleaning

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAI1e2OVnHLyYlj4uCoBHFGE5OG9wSAAUI",
+    authDomain: "clone-281bc.firebaseapp.com",
+    databaseURL: "https://clone-281bc.firebaseio.com",
+    projectId: "clone-281bc",
+    storageBucket: "clone-281bc.appspot.com",
+    messagingSenderId: "478725925401",
+    appId: "1:478725925401:web:e63dfc5eb4422ed6215888",
+    measurementId: "G-27BXLNMZ4G"
+  };
