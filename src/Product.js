cd2361551ac917df8a81a46334aae93c7980f351
Gaurav Dubey <gauravd638@gmail.com>
2020-10-26 02:28:13 +0530

    adding basket content with contex api

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import "./Product.css";
+import {useStateValue} from "./StateProvider";
 
 function Product({id,title,image,price,rating}) {
+
+    const [state,dispatch] = useStateValue();
+    console.log("this is the basket >>>");
     const addToBasket=()=>{
         //dispatch the item into data layer
-    }
+        dispatch({
+            type:"ADD_TO_BASKET",
+            item:{
+                id:id,
+                title:title,
+                image:image,
+                price:price,
+                rating:rating,
+            },
+        });
+
+    };
     return (
         <div className="product">
             <div className="product__info">
